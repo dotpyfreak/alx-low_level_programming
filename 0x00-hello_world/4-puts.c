ff59a786a17cfad2cba8d55f19cf7b7f72c3dedd
Joses Peter <josespeter12@gmail.com>
2022-12-08 16:48:27 -0800

    printiting to stdout using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	/*main -Entry point
-	 *
-	 * Retun: always 0
-	 * */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
