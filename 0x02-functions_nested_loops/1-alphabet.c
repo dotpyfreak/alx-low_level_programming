ff87d274600c2d095aa9471f7306e85f3cc55b2b
Joses Peter <josespeter12@gmail.com>
2022-12-13 12:19:25 -0800

    updated main.h and added a file to print letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the letters of the alphabets
+ * Return: void (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
