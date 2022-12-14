7c48b8049363afc0f88ffaabb37e176c5f1b1c7e
dotpyfreak <josespeter12@gmail.com>
2022-12-14 02:06:35 +0100

    print every minute of the day from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 24)
+	{
+		int j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0')
+			_putchar((i % 10) + '0')
+			_putchar(':')
+			_putchar((j / 10) + '0')
+			_putchar((j % 10') + '0')
+			j++;
+		}
+		i++;
+	}
+}
