f6be9212b6f66d8ff5c076ac61445d62b3bcd818
dotpyfreak <josespeter12@gmail.com>
2022-12-14 03:47:33 +0100

    prints ints to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * followed by a new line
+ * @n: any given int
+ * Return: An Int
+ */
+
+void print_to_98(int n)
+{
+	if (x <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x != 98)
+				printf("%d, ", x)
+			else
+				printf("%d\n", x)
+		}
+	}
+	else if (x >= 98)
+	{
+		for (x = n; x >= 98; x--)
+		{
+			if (x != 98)
+				printf("%d, ", x)
+			else
+				printf("%d\n", x)
+		}
+	}
+}
