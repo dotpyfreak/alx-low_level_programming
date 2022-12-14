afe90fda902f19dc8d8e761001bcaf77add67556
dotpyfreak <josespeter12@gmail.com>
2022-12-14 04:45:47 +0100

    prints sum of multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of multiples of 3 or 5 below 1024
+ * Return: int
+ */
+
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 0;
+	c = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			a = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			b = b + i;
+		}
+	}
+	c = sum3 + b;
+	printf("%lu\n", c);
+	return (0);
+}
