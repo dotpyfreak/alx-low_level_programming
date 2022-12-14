ccf0d7ea921f9981f2baa54d58d15a5871519083
dotpyfreak <josespeter12@gmail.com>
2022-12-14 04:33:04 +0100

    prints n multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,10 @@
 
 void print_times_table(int n)
 {
+	int i, j, k;
+
 	if (n <= 15 && n >= 0)
 	{
-		int i, j, k;
-
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
@@ -21,18 +21,26 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-
-				if (k < n && j != 0)
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (k >= n)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
