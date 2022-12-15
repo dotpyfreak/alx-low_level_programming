d0c0b08c26f029b9046b502db5b782e137a754bc
dotpyfreak <josespeter12@gmail.com>
2022-12-16 00:23:20 +0100

    to print digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,11 @@
-#inlclude "main.h"
+#include "main.h"
 
 /**
- * _isdigit - Checks for a digit ( 0 through 9)
- * @c: any given digit
- * Return: 1 if c is a digit and 0 otherwise
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: integer to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
-
 int _isdigit(int c)
 {
-	return ((c >= 48) && (c <= 57));
+	return (c >= '0' && c <= '9');
 }
