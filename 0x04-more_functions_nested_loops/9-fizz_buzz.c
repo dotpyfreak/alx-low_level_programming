d2af6f9412b9a4b8570f39bed0ce45085cae6328
dotpyfreak <josespeter12@gmail.com>
2022-12-16 01:53:50 +0100

    fizz buzz modifications

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,21 @@
 
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
+		if ((i % 15) == 0)
 			printf("FizzBuzz ");
-		else if (a % 3 == 0 && a % 5 != 0)
-			printf("Fizz ")
-		else if (a % 5 == 0 && a % 3 != 0)
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
 			printf("Buzz ");
-		else if (a % 3 != 0 && a % 5 != 0)
-			printf("%d ", a);
+		else
+			printf("%d ", i);
+
 	}
-	printf('\n');
-	
+	printf("\n");
+
 	return (0);
 }
-
