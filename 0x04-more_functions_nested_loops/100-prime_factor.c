e4cc3e13f0624ed6748eff30861948ea0c012c98
dotpyfreak <josespeter12@gmail.com>
2022-12-16 02:48:04 +0100

    prints the largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest prime factor of any given number
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	long int n;
+
+	n = 612852475143;
+
+	long int div = 2, ans = 0, maxi;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+			div = div + 1;
+		else
+		{
+			maxi = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%d",maxi);
+				printf("\n")
+				ans = 1;
+				break;
+			}
+
+
+	return (0);
+	}
+
+}
+
+}
