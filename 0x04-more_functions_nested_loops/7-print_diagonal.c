fefb44a7953cc7b5f90d2a1fee89053d9dfb873f
dotpyfreak <josespeter12@gmail.com>
2022-12-16 01:01:11 +0100

    prints a diagonal line of length n

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,19 +8,19 @@
 
 void print_diagonal(int n)
 {
-	int a, b;
-
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
+		int a, b;
+
 		for (a = 0; a < n; a++)
 		{
 			for (b = 0; b < n; b++)
 			{
-				if (b != a)
+				if (b < a)
 					_putchar(' ');
-				else
+				else if (b == a)
 					_putchar('\\');
 			}
 		_putchar('\n');
