755af179f5aa77e6b924d6822f84446e7064ad60
dotpyfreak <josespeter12@gmail.com>
2022-12-16 03:20:33 +0100

    prints the largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,31 +8,29 @@
 
 int main(void)
 {
-	long int n;
+	long int n, m, i;
 
 	n = 612852475143;
+	m = -1;
 
-	long int div = 2, ans = 0, maxi;
-
-	while (n != 0)
+	while (n % 2 == 0)
 	{
-		if (n % div != 0)
-			div = div + 1;
-		else
-		{
-			maxi = n;
-			n = n / div;
-			if (n == 1)
-			{
-				printf("%d", maxi);
-				printf("\n")
-				ans = 1;
-				break;
-			}
-
-		}
-
-	return (0);
+		m = 2;
+		n /= 2;
 	}
 
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			m = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+		m = n;
+	printf("%ld\n", m)
+
+	return (0);
 }
+
