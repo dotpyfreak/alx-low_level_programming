145764a993ce9b108f90fcec917d446cc181e2bc
dotpyfreak <josespeter12@gmail.com>
2022-12-20 03:56:16 +0100

    now we can print n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: a given array
+ * @n: number of elements of the array to be printed
+ * Return: prints n elements
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	if (i < n - 1)
+	{
+		for (i = 0; i < n; i++)
+			printf("%d, ", a[i]);
+	}
+	else if (i == n - 1)
+		printf("%d", a[i]);
+	printf("\n");
+}
