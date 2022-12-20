e9053aff9752f24c93e433f30997e548d8c7101f
dotpyfreak <josespeter12@gmail.com>
2022-12-20 04:13:32 +0100

    now we can print n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,13 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (i != n - 1)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		if (i != (n - 1)
 			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 	}
-	else
-		printf("%d", a[i]);
+
 	printf("\n");
 }
