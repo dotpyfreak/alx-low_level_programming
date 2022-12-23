0a91b9388410c016b94e6890df189eebd506978f
dotpyfreak <josespeter12@gmail.com>
2022-12-23 23:29:19 +0100

    now we can print in Upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,11 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (*s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (*s[i] >= 97 && *s[i] <= 122)
-			*s[i] = *s[i] - 32;
-		*s++;
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
 	}
-	return (*s);
+	return (s);
 }
