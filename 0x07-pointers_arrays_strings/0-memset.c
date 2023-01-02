a8a2cff8c59c16c8ec563572d03e7d82760fce88
dotpyfreak <josespeter12@gmail.com>
2023-01-02 23:41:16 +0100

    Learning about memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,15 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
-	unsigned char *p = s;
+	unsigned int i = 0;
 
-	i = 0;
-	while (n > 0)
+	while (n > i)
 	{
-		*p = b;
-		p++;
-		n--;
+		s[i] = b;
+		i++;
 	}
 	return (s);
 }
