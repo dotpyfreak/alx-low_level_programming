106df892ec5eeade7c988c6984cdda93335018bf
dotpyfreak <josespeter12@gmail.com>
2023-01-02 23:19:22 +0100

    Learning about memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with constant byte
+ * @s: a pointer to the memory area
+ * @b: constant byte to be filled with
+ * @n: first number of bytes of the memory area pointed to by s
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	unsigned char *p = s;
+	i = 0;
+	while(n > 0)
+	{
+		*p = b;
+		p++;
+		n--;
+	}
+	return(s);
+}
