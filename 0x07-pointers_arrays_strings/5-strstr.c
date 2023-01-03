c587b640f264b31be73d79f4a5bfa01d12fdd775
dotpyfreak <josespeter12@gmail.com>
2023-01-03 02:04:01 +0100

    learning strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - this function locates a substring
+ * @haystack: This is the main string to be examined
+ * @needle: This is the substring to be examined
+ * Return: a pointer to the beginning of the located substring or NULL
+ * if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		hay = haystack;
+		nee = needle;
+
+		while (*nee != '\0' && *haystack == *nee)
+		{
+			haystack++;
+			nee++;
+		}
+		if(!*nee)
+			return (hay);
+		haystack++;
+	}
+	return ('\0')
+}
