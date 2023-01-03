435f0865112ff3925bc6e0236ec8ab5367e24856
dotpyfreak <josespeter12@gmail.com>
2023-01-03 03:22:29 +0100

    to change a pointer's value to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - function sets the value of a pointer to a char
+ * @s: a pointer to a pointer
+ * @to: a pointer
+ * Return: the pointer value as a char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
