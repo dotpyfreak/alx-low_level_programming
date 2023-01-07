f13e5e804706443f7d4a1d3d1de9144825b36f1f
dotpyfreak <josespeter12@gmail.com>
2023-01-07 03:43:54 +0100

    add positive numbers only

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,10 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	int i, sum = 0;
+	int i, j, add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		int j = 0;
-
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
@@ -31,14 +23,8 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		int n;
-
-		n = atoi(argv[i]);
-		if (n > 0)
-		{
-			sum += n;
-		}
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
