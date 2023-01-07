049ee7f115599fc100009cc65ab5d7cf0d9e0600
dotpyfreak <josespeter12@gmail.com>
2023-01-07 02:07:58 +0100

    we are counting arguments now

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints the
+ * number of arguments passed to it
+ * @argc: the number of arguments in argv
+ * @argv: an array of strings
+ * Return: an int, the number of arguments
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
