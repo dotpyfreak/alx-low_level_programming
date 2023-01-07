dd23d97935f94ecb478ba7dd8cc3a56f119e8dd9
dotpyfreak <josespeter12@gmail.com>
2023-01-07 01:57:51 +0100

    hey program, what's your name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its name,
+ * followed by a new line
+ * @argc: the number of strings in argv
+ * @argv: an array of strings
+ * Return: The program's name
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
