a50af25db30ec8756d04437b7b4cdcf1758cfc75
dotpyfreak <josespeter12@gmail.com>
2023-01-07 02:51:25 +0100

    prints the product of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,11 @@
 
 int main(int argc, char *argv[])
 {
-	int i, mul = 1;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-			mul *= atoi(argv[i]);
-	}
-		printf("%d\n", mul);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
