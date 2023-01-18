59423f748949317f90ffa5771e754cada491bc96
dotpyfreak <josespeter12@gmail.com>
2023-01-18 03:56:36 +0100

    structs and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
-
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(const char *str);
+ar *_strcopy(char *dest, char *src);
 #endif /* _DOG_H */
